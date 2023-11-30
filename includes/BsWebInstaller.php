151ad5a4ceb8c3152e2d665a2eb0406088e53a3a
akulbii <alk@astarios.com>
2023-11-30 13:05:09 +0200

    Remove content provisioning from install step
    There are few problems with content provisioning during "maintenance/install.php":
    * "Property" namespace does not exist at this stage because SMW is not loaded yet. That's why provisioned pages with "Property" namespace will be created in main namespace, which is wrong.
    * When "maintenance/update.php" is executed first time, there is plenty of MediaWiki maintenance scripts which are executed. One of them, "deleteDefaultMessages.php", removes all pages in "MediaWiki" namespace which were last edited by "MediaWiki default" user. As soon as "DefaultContentProvisioner" uses exactly this user - all provisioned in "MediaWiki" namespace pages will be deleted when executing "update.php".
    
    [4.4.x]
    
    ERM35085
    
    Change-Id: I0b7e5032bde80562a4ab05baf8180d73c53c1d37
    (cherry picked from commit 626da573310ca4b766bae7ce84664b93cbf6a59c)
    (cherry picked from commit d84ccb620a308803c7270b4062249aa18ece4fce)

--- includes/BsWebInstaller.php
+++ includes/BsWebInstaller.php
@@ -81,8 +81,7 @@ class BsWebInstaller extends WebInstaller {
 	protected function getInstallSteps( DatabaseInstaller $installer ) {
 		$this->installSteps = parent::getInstallSteps( $installer );
 		$bsInstallSteps = [
-			[ 'name' => 'sidebar', 'callback' => [ $this, 'createSidebar' ] ],
-			[ 'name' => 'default-bs-content', 'callback' => [ $this, 'createDefaultBsContent' ] ]
+			[ 'name' => 'sidebar', 'callback' => [ $this, 'createSidebar' ] ]
 		];
 
 		// Add BlueSpice install steps to the core install list,
@@ -262,53 +261,6 @@ class BsWebInstaller extends WebInstaller {
 		} catch ( Exception $e ) {
 			// using raw, because $wgShowExceptionDetails can not be set yet
 			$status->fatal( 'config-install-sidebar-failed', $e->getMessage() );
-		}
-
-		return $status;
-	}
-
-	/**
-	 * Processes manifests with default BlueSpice content and imports it.
-	 *
-	 * @param DatabaseInstaller $installer
-	 * @return Status
-	 */
-	protected function createDefaultBsContent( DatabaseInstaller $installer ) {
-		$installPath = $this->getVar( 'IP' );
-		$exts = $this->getVar( '_Extensions' );
-
-		$processedExtensions = [];
-		foreach ( $exts as $e ) {
-			$processedExtensions[] = $e;
-		}
-
-		// Some imported templates may contain "*.css" subpages containing CSS styles for that template
-		// Example: "Template:SomeTemplate/styles.css"
-		// Such "*.css" wiki pages are handled by "sanitized-css" content model from "TemplateStyles" extension
-		// That "sanitized-css" content model and corresponding handler are registered via hook in "TemplateStyles".
-		// But, as soon as installer doesn't know anything about hooks from extensions, we cannot apply that model here.
-		// In such cases "wikitext" content model will be applied to "Template:*/*.css" pages.
-		// As a result, CSS will be outputted as wikitext - that's wrong.
-
-		// So we need to mock "sanitized-css" content model here
-		$this->mockSanitizedCssContentModel();
-
-		$objectFactory = MediaWikiServices::getInstance()->getObjectFactory();
-
-		$contentProvisionerRegistry = new FileBasedRegistry( $processedExtensions, $installPath );
-
-		$contentProvisionerPipeline = new ContentProvisionerPipeline( $objectFactory, $contentProvisionerRegistry );
-		$contentProvisionerPipeline->setLogger( LoggerFactory::getInstance( 'ContentProvisioner' ) );
-		$contentProvisionerPipeline->setOutput( new PrintOutput() );
-
-		try {
-			$this->output->startLiveBox();
-
-			$status = $contentProvisionerPipeline->execute();
-		} catch ( Exception $e ) {
-			$status = Status::newFatal( 'config-install-default-bs-content-failed', $e->getMessage() );
-		} finally {
-			$this->output->endLiveBox();
 		}
 
 		return $status;
